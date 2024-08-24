6367939f176a0e2c088c3ab719370b41cb1af8e0
Oleksii Ozyrskyi <165183808+lexxus0@users.noreply.github.com>
2024-08-24 16:05:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
-  
-
-
-  // const filteredContacts = contacts.filter((contact) =>
-  //   contact.name.toLowerCase().includes(searchValue.toLowerCase())
-  // );
-
-
-
   return (
     <div>
       <h1>Phonebook</h1>
